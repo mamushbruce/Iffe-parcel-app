74e0b688f719a6e0c1b2e16c6c6ff7e47d2cf5b0
Cosmic Silence <nsambaevans0@gmail.com>
2026-01-10 16:22:20 +0000

    add padding below (_for element section_)

--- src/app/ideas/page.tsx
+++ src/app/ideas/page.tsx
@@ -299,7 +299,7 @@ export default function IdeaBoxPage() {
           </div>
       </section>
 
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pb-8">
         {ideas.map(idea => (
           <AnimatedIdeaCard key={idea.id} idea={idea} />
         ))}
